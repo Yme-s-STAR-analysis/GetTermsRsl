17cc4c5d7914dbd95637236e73de73c5d3db184f
Yige <huangyige@impcas.ac.cn>
2025-11-24 20:58:26 +0800

    <feat> homo8.0: Dataset configuration embedded. The efficiency configurations are stated for each dataset/energy, respectively.

--- Source/utils/EffMaker.cxx
+++ Source/utils/EffMaker.cxx
@@ -6,6 +6,71 @@
 #include "TString.h"
 #include <iostream>
 
+bool EffMaker::Init(std::string energy, const char* sysTag, const char* nSigTag) {
+    this->energy = energy;
+    if (energy == "7.7") {
+        ReadInEffFile(
+            Form("/star/u/yghuang/Work/DataAnalysis/BES2/OverAll/4EmbedList/4EffFiles/7/TpcEff.%s.root", sysTag),
+            Form("/star/u/yghuang/Work/DataAnalysis/BES2/OverAll/4EmbedList/4EffFiles/7/TofEff.%s.root", sysTag),
+            "/star/u/yghuang/Work/DataAnalysis/BES2/OverAll/4EmbedList/4EffFiles/7/PidEff.root",
+            nSigTag
+        );
+    } else if (energy == "9.2") {
+        std::cout << "[LOG] - From EffMaker Module: Initialize EffMaker with energy " << energy << "." << std::endl;
+        ReadInEffFile(
+            Form("/star/u/yghuang/Work/DataAnalysis/BES2/OverAll/4EmbedList/4EffFiles/9/TpcEff.%s.root", sysTag),
+            Form("/star/u/yghuang/Work/DataAnalysis/BES2/OverAll/4EmbedList/4EffFiles/9/TofEff.%s.root", sysTag),
+            "/star/u/yghuang/Work/DataAnalysis/BES2/OverAll/4EmbedList/4EffFiles/9/PidEff.root",
+            nSigTag
+        );
+    } else if (energy == "11.5") {
+        std::cout << "[LOG] - From EffMaker Module: Initialize EffMaker with energy " << energy << "." << std::endl;
+        ReadInEffFile(
+            Form("/star/u/yghuang/Work/DataAnalysis/BES2/OverAll/4EmbedList/4EffFiles/11/TpcEff.%s.root", sysTag),
+            Form("/star/u/yghuang/Work/DataAnalysis/BES2/OverAll/4EmbedList/4EffFiles/11/TofEff.%s.root", sysTag),
+            "/star/u/yghuang/Work/DataAnalysis/BES2/OverAll/4EmbedList/4EffFiles/11/PidEff.root",
+            nSigTag
+        );
+    } else if (energy == "14.6") {
+        std::cout << "[LOG] - From EffMaker Module: Initialize EffMaker with energy " << energy << "." << std::endl;
+        ReadInEffFile(
+            Form("/star/u/yghuang/Work/DataAnalysis/BES2/OverAll/4EmbedList/4EffFiles/14/TpcEff.%s.root", sysTag),
+            Form("/star/u/yghuang/Work/DataAnalysis/BES2/OverAll/4EmbedList/4EffFiles/14/TofEff.%s.root", sysTag),
+            "/star/u/yghuang/Work/DataAnalysis/BES2/OverAll/4EmbedList/4EffFiles/14/PidEff.root",
+            nSigTag
+        );
+    } else if (energy == "17.3") {
+        std::cout << "[LOG] - From EffMaker Module: Initialize EffMaker with energy " << energy << "." << std::endl;
+        ReadInEffFile(
+            Form("/star/u/yghuang/Work/DataAnalysis/BES2/OverAll/4EmbedList/4EffFiles/17/TpcEff.%s.root", sysTag),
+            Form("/star/u/yghuang/Work/DataAnalysis/BES2/OverAll/4EmbedList/4EffFiles/17/TofEff.%s.root", sysTag),
+            "/star/u/yghuang/Work/DataAnalysis/BES2/OverAll/4EmbedList/4EffFiles/17/PidEff.root",
+            nSigTag
+        );
+    } else if (energy == "19.6") {
+        std::cout << "[LOG] - From EffMaker Module: Initialize EffMaker with energy " << energy << "." << std::endl;
+        ReadInEffFile(
+            Form("/star/u/yghuang/Work/DataAnalysis/BES2/OverAll/4EmbedList/4EffFiles/19/TpcEff.%s.root", sysTag),
+            Form("/star/u/yghuang/Work/DataAnalysis/BES2/OverAll/4EmbedList/4EffFiles/19/TofEff.%s.root", sysTag),
+            "/star/u/yghuang/Work/DataAnalysis/BES2/OverAll/4EmbedList/4EffFiles/19/PidEff.root",
+            nSigTag
+        );
+    } else if (energy == "27") {
+        std::cout << "[LOG] - From EffMaker Module: Initialize EffMaker with energy " << energy << "." << std::endl;
+        ReadInEffFile(
+            Form("/star/u/yghuang/Work/DataAnalysis/BES2/OverAll/4EmbedList/4EffFiles/27/TpcEff.%s.root", sysTag),
+            Form("/star/u/yghuang/Work/DataAnalysis/BES2/OverAll/4EmbedList/4EffFiles/27/TofEff.%s.root", sysTag),
+            "/star/u/yghuang/Work/DataAnalysis/BES2/OverAll/4EmbedList/4EffFiles/27/PidEff.root",
+            nSigTag
+        );
+    } else {
+        std::cout << "[LOG] - From EffMaker Module: Initializaion encountered with an invalid enenrgy " << energy << "." << std::endl;
+        return false;
+    }
+    std::cout << "[LOG] - From EffMaker Module: Initialize EffMaker with energy " << energy << "." << std::endl;
+    return true;
+}
+
 void EffMaker::ReadInEffFile(const char* tpc, const char* tof, const char* pid, const char* nSigTag) {
     TFile* tf_tpc = 0;
     TFile* tf_tof = 0;
@@ -38,36 +103,58 @@ void EffMaker::ReadInEffFile(const char* tpc, const char* tof, const char* pid, 
     for (int iVz=0; iVz<nVz; iVz++) {
         for (int iCent=0; iCent<nCent; iCent++) {
             for (int iY=0; iY<nY; iY++) {
+                // skip iVz >= 3 and iY >= 10 bins for 27 GeV
+                if (energy == "27") { if (iVz > 2 || iY > 9) { continue; } }
                 if (!tpcOff) {
-                    tf_tpc->GetObject(
-                        Form("TpcEff_cent%d_vz%d_y%d_Pro", iCent, iVz, iY),
-                        ftpc_pro[iCent][iVz][iY]
-                    );
-                    tf_tpc->GetObject(
-                        Form("TpcEff_cent%d_vz%d_y%d_Pbar", iCent, iVz, iY),
-                        ftpc_pbar[iCent][iVz][iY]
-                    );
+                    if (energy != "9.2") {
+                        tf_tpc->GetObject(
+                            Form("TpcEff_cent%d_vz%d_y%d_Pro", iCent, iVz, iY),
+                            ftpc_pro[iCent][iVz][iY][0]
+                        );
+                        tf_tpc->GetObject(
+                            Form("TpcEff_cent%d_vz%d_y%d_Pbar", iCent, iVz, iY),
+                            ftpc_pbar[iCent][iVz][iY][0]
+                        );
+
+                    } else { // for 9.2 GeV, we have 2 regions using different TPC tracking efficiency
+                        tf_tpc->GetObject(
+                            Form("TpcEff_cent%d_vz%d_y%d_Pro_0", iCent, iVz, iY),
+                            ftpc_pro[iCent][iVz][iY][0]
+                        );
+                        tf_tpc->GetObject(
+                            Form("TpcEff_cent%d_vz%d_y%d_Pbar_0", iCent, iVz, iY),
+                            ftpc_pbar[iCent][iVz][iY][0]
+                        );
+                        tf_tpc->GetObject(
+                            Form("TpcEff_cent%d_vz%d_y%d_Pro_1", iCent, iVz, iY),
+                            ftpc_pro[iCent][iVz][iY][1]
+                        );
+                        tf_tpc->GetObject(
+                            Form("TpcEff_cent%d_vz%d_y%d_Pbar_1", iCent, iVz, iY),
+                            ftpc_pbar[iCent][iVz][iY][1]
+                        );
+                    }
                 }
                 if (!tofOff) {
-#ifndef __INTERPOLATE_TOF_EFF__
-                    tf_tof->GetObject(
-                        Form("TofEff_cent%d_vz%d_y%d_Pro", iCent, iVz, iY),
-                        ftof_pro[iCent][iVz][iY]
-                    );
-                    tf_tof->GetObject(
-                        Form("TofEff_cent%d_vz%d_y%d_Pbar", iCent, iVz, iY),
-                        ftof_pbar[iCent][iVz][iY]
-                    );
-#else
-                    tf_tof->GetObject(
-                        Form("TofEff_cent%d_vz%d_y%d_Pro", iCent, iVz, iY),
-                        htof_pro[iCent][iVz][iY]
-                    );
-                    tf_tof->GetObject(
-                        Form("TofEff_cent%d_vz%d_y%d_Pbar", iCent, iVz, iY),
-                        htof_pbar[iCent][iVz][iY]
-                    );
-#endif
+                    if (energy == "7.7" || energy == "9.2" || energy == "11.5") {
+                        tf_tof->GetObject(
+                            Form("TofEff_cent%d_vz%d_y%d_Pro", iCent, iVz, iY),
+                            htof_pro[iCent][iVz][iY]
+                        );
+                        tf_tof->GetObject(
+                            Form("TofEff_cent%d_vz%d_y%d_Pbar", iCent, iVz, iY),
+                            htof_pbar[iCent][iVz][iY]
+                        );
+                    } else {
+                        tf_tof->GetObject(
+                            Form("TofEff_cent%d_vz%d_y%d_Pro", iCent, iVz, iY),
+                            ftof_pro[iCent][iVz][iY]
+                        );
+                        tf_tof->GetObject(
+                            Form("TofEff_cent%d_vz%d_y%d_Pbar", iCent, iVz, iY),
+                            ftof_pbar[iCent][iVz][iY]
+                        );
+                    }
                 }
             }
 
@@ -94,9 +181,9 @@ double EffMaker::GetTpcEff(bool positive, double pt, double y, int cent, double 
     int yb = YPSplit(y);
     if (yb < 0) { return -1; }
     if (positive) {
-        ff = ftpc_pro[cent][vz][yb];
+        ff = ftpc_pro[cent][vz][yb][region];
     } else {
-        ff = ftpc_pbar[cent][vz][yb];
+        ff = ftpc_pbar[cent][vz][yb][region];
     }
     double eff = ff->Eval(pt);
     if (eff < 0 || eff > 1) { return -1; }
@@ -111,19 +198,19 @@ double EffMaker::GetTofEff(bool positive, double pt, double y, int cent, double 
     int yb = YPSplit(y);
     if (yb < 0) { return -1; }
     double eff = -1;
-#ifndef __INTERPOLATE_TOF_EFF__
-    if (positive) {
-        eff = ftof_pro[cent][vz][yb]->Eval(pt);
+    if (energy == "7.7" || energy == "9.2" || energy == "11.5") {
+        if (positive) {
+            eff = htof_pro[cent][vz][yb]->Interpolate(pt);
+        } else {
+            eff = htof_pbar[cent][vz][yb]->Interpolate(pt);
+        }
     } else {
-        eff = ftof_pbar[cent][vz][yb]->Eval(pt);
+        if (positive) {
+            eff = ftof_pro[cent][vz][yb]->Eval(pt);
+        } else {
+            eff = ftof_pbar[cent][vz][yb]->Eval(pt);
+        }
     }
-#else
-    if (positive) {
-        eff = htof_pro[cent][vz][yb]->Interpolate(pt);
-    } else {
-        eff = htof_pbar[cent][vz][yb]->Interpolate(pt);
-    }
-#endif
     if (eff < 0 || eff > 1) { return -1; }
     return eff;
 }
@@ -148,25 +235,37 @@ int EffMaker::VzSplit(double vz) {
         This depends on your vz split method.
         -1 means invalid vz
     */
-    if (-30 < vz && vz < -10) {
-        return 0;
-    } else if (-10 < vz && vz < 10) {
-        return 1;
-    } else if (10 < vz && vz < 30) {
-        return 2;
-    } else if (-50 < vz && vz < -30) {
-        return 3;
-    } else if (30 < vz && vz < 50) {
-        return 4;
+    if (energy == "27") { // only 3 Vz bins for 27 GeV
+        if (-27 < vz && vz < -10) {
+            return 0;
+        } else if (-10 < vz && vz < 10) {
+            return 1;
+        } else if (10 < vz && vz < 27) {
+            return 2;
+        } else {
+            return -1;
+        }
     } else {
-        return -1;
+        if (-30 < vz && vz < -10) {
+            return 0;
+        } else if (-10 < vz && vz < 10) {
+            return 1;
+        } else if (10 < vz && vz < 30) {
+            return 2;
+        } else if (-50 < vz && vz < -30) {
+            return 3;
+        } else if (30 < vz && vz < 50) {
+            return 4;
+        } else {
+            return -1;
+        }
     }
 }
 
 int EffMaker::YPSplit(double y) {
     /*
-        v3.0 new
-        v3.5 2 more bins
+        10 bins for 27 GeV, and
+        12 bins for other enenrgies
     */
    if (-0.5 < y && y < -0.4) { return 0; }
    else if (-0.4 < y && y < -0.3) { return 1; }
@@ -178,7 +277,7 @@ int EffMaker::YPSplit(double y) {
    else if (0.2 < y && y < 0.3) { return 7; }
    else if (0.3 < y && y < 0.4) { return 8; }
    else if (0.4 < y && y < 0.5) { return 9; }
-   else if (-0.6 < y && y < -0.5) { return 10; }
-   else if (0.5 < y && y < 0.6) { return 11; }
+   else if (-0.6 < y && y < -0.5 && energy != "27") { return 10; }
+   else if (0.5 < y && y < 0.6 && energy != "27") { return 11; }
    else { return -1; }
 }
--- Source/utils/EffMaker.h
+++ Source/utils/EffMaker.h
@@ -1,5 +1,7 @@
-#ifndef YIGE_EFF_MAKER_HEADER
-#define YIGE_EFF_MAKER_HEADER
+#ifndef __RSL_EFFMAKER__
+#define __RSL_EFFMAKER__
+
+#include <string>
 
 class TH1D;
 class TH2F;
@@ -9,22 +11,27 @@ class EffMaker{
     private:
         static const int nCent = 9;
         static const int nVz = 5;
+        static const int nRegion = 2; // this is for 9.2 GeV
 
+        int region;
+        std::string energy;
         TH2F* pid_pro;
         TH2F* pid_pbar;
 
         TH2F* th2;
 
         static const int nY = 12;
-        TF1* ftpc_pro[nCent][nVz][nY];
-        TF1* ftpc_pbar[nCent][nVz][nY];
-#ifdef __INTERPOLATE_TOF_EFF__
+        // TPC efficiency, two-region design is only for 9.2 GeV
+        TF1* ftpc_pro[nCent][nVz][nY][nRegion];
+        TF1* ftpc_pbar[nCent][nVz][nY][nRegion];
+
+        // TOF efficiency, bin-by-bin for enenrgy >= 14.6
         TH1D* htof_pro[nCent][nVz][nY];
         TH1D* htof_pbar[nCent][nVz][nY];
-#else
+        // TOF efficiency, interpolation for enenrgy <= 11.5
         TF1* ftof_pro[nCent][nVz][nY];
         TF1* ftof_pbar[nCent][nVz][nY];
-#endif
+
         TF1* ff;
 
         bool tpcOff;
@@ -32,17 +39,17 @@ class EffMaker{
         bool pidOff;
 
     public:
-        EffMaker(){}
+        EffMaker():region(0){}
         ~EffMaker(){}
 
+        bool Init(std::string energy, const char* sysTag,const char* nSigTag);
         void ReadInEffFile(const char* tpc, const char* tof, const char* pid, const char* nSigTag);
         double GetTpcEff(bool positive, double pt, double y, int cent, double vz);
         double GetTofEff(bool positive, double pt, double y, int cent, double vz);
-        // double GetPidEff(bool positive, double p, double vz);
         double GetPidEff(bool positive, double pt, double y, bool asCut);
-        // version 3 new
         int YPSplit(double y);
         int VzSplit(double vz);
+        void SetRegion(int region) { this->region = region; } // for 9.2 GeV, call this in Core.cxx when for each femto-event
 };
 
 #endif
